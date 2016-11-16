0fc63f24e66a635d373777c7aab81cef91cf1064
matheussn <matheusneto23@gmail.com>
2016-11-16 13:13:37 -0200

    teste funcao atoi

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
-#include <stdlib.h>
+/*
+ * Grupo HMR
+ * Integrantes
+ * Hugo Sousa Nasciutti - 11621BSI260
+ * Matheus Santiago Neto - 11621BSI252
+ * Rodrigo Souza Rezende - 
+*/
+
 #include <stdio.h>
+#include <stdlib.h> // atoi
 
 int main()
 {
-  return 0;
+    int i;
+    char str[10];
+
+    printf ("Digite um numero: ");
+    fgets(str, 10, stdin);
+    i = atoi (str);
+    printf ("O valor que voce digitou eh %d \n",i);
+    return 0;
 }
